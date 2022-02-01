e9eec115f58a64af891a739eb3a951ef1708bd98
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-02-01 10:08:34 +0200

    added style for not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,17 @@
+//router
+import { useNavigate } from "react-router-dom";
+
+
 const NotFound = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <div>
-            404 not found
+        <div className="notFoundContainer">
+            <div className="notFoundBox">
+                <h1 className="notFoundTitle">404 Page Not Found</h1>
+                <button className="btn btn-primary notFoundBtn" onClick={() => navigate('/')}>Go to home</button>
+            </div>
         </div>
     );
 }
